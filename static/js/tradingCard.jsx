e60cb56b4e22962bb8e50f4f16def7457a59f6f6
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-31 12:38:02 -0700

    Add skills; add trading cards

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -4,7 +4,7 @@ class TradingCard extends React.Component {
 			<div className="card">
 				<h2>Name: {this.props.name}</h2>
 				<img src={this.props.imgUrl} />
-				<h2>Skill: </h2>
+				<h2>Skill: {this.props.skill}</h2>
 			</div>
 		);
 	}
@@ -23,4 +23,15 @@ ReactDOM.render(
 ReactDOM.render(
 	<TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
 	document.getElementById('llambda')
+);
+
+ReactDOM.render(
+	<TradingCard name="Merge" skill="eating candy" imgUrl="/static/img/merge.jpg" />,
+	document.getElementById('merge')
+);
+
+ReactDOM.render(
+	<TradingCard name="Polymorphism" skill="blending in" imgUrl="/static/img/polymorphism.jpg" />,
+	document.getElementById('polymorphism')
+
 );
